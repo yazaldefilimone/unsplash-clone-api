f849558d6c6d2c77fc6f296837c85bb8f15e1cf8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:49:22 +0200

    feat; register routes images

--- src/main/settings/app.ts
+++ src/main/settings/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import helmet from "helmet";
-import { userRoutes } from "../routes/user";
+import { imageRoutes } from "@/main/routes/image";
+import { userRoutes } from "@/main/routes/user";
+
 const app = express();
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/user", userRoutes);
+app.use("/image", imageRoutes);
 export { app };
