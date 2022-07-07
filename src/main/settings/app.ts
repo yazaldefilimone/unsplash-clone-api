e7837996d7d4015b4b7f570efc0e6137759ce63c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:51:22 +0200

    feat: add cors

--- src/main/settings/app.ts
+++ src/main/settings/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import helmet from "helmet";
 import { imageRoutes } from "@/main/routes/image";
 import { userRoutes } from "@/main/routes/user";
+import cors from "cors";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use("/user", userRoutes);
 app.use("/image", imageRoutes);
 export { app };
