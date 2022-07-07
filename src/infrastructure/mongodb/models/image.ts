0127004ad24b8817ea0b44f0f4ee39de72e5a03f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:41:57 +0200

    fix: rename method of user

--- src/infrastructure/mongodb/models/image.ts
+++ src/infrastructure/mongodb/models/image.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const imageSchema = new Schema({
   label: String,
   url: String,
-  user_id: String,
+  userId: String,
 });
 
 export const imageModel = mongoose.model("Image", imageSchema);
