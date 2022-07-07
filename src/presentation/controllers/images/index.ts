2f6d418bd69e07a0ba4df314212acb7819b10160
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:48:33 +0200

    style: export method

---
+++ src/presentation/controllers/images/index.ts
@@ -0,0 +1,5 @@
+export * from "./CreateImgeController";
+export * from "./DeleteImageController";
+export * from "./FindAllImageController";
+export * from "./FindOneImageController";
+export * from "./FindUserImageController";
