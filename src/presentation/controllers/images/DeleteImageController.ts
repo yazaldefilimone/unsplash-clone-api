0d1a4d2977b878424d61cb685a15213c225ebba1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:42:28 +0200

    feat: delete image controller

---
+++ src/presentation/controllers/images/DeleteImageController.ts
@@ -0,0 +1,22 @@
+import { IDeleteImageUseCase } from "@/domain/usecases/image";
+import { Request, Response } from "express";
+
+export class DeleteImageController {
+  private readonly deleteImageUseCase: IDeleteImageUseCase;
+  constructor(deleteImageUseCase: IDeleteImageUseCase) {
+    this.deleteImageUseCase = deleteImageUseCase;
+  }
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { body } = request;
+      const result = await this.deleteImageUseCase.perform(body);
+
+      if (result.isLeft()) {
+        return response.status(400).json({ message: result.value.message });
+      }
+      return response.status(200).send();
+    } catch (error: any) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
