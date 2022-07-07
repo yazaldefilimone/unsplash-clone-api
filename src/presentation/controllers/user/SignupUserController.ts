5a4723a9ba87ad2f254ae83a6d49651eafcf7f80
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 09:33:05 +0200

    feat: create signup user controller

---
+++ src/presentation/controllers/user/SignupUserController.ts
@@ -0,0 +1,23 @@
+import { ISignupUserUseCase } from "@/domain/usecases/user";
+import { Request, response, Response } from "express";
+
+export class SignupUserController {
+  private readonly signupUserUseCase: ISignupUserUseCase;
+  constructor(signupUserUseCase: ISignupUserUseCase) {
+    this.signupUserUseCase = signupUserUseCase;
+  }
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { body } = request;
+      console.log({ body });
+      const result = await this.signupUserUseCase.perform(body);
+
+      if (result.isLeft()) {
+        return response.status(401).json({ message: result.value.message });
+      }
+      return response.status(201).send(result.value);
+    } catch (error: any) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
