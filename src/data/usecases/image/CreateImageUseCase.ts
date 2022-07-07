93be7c8df0808e19d51078494f906c07587373a5
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 10:12:19 +0200

    feat: create image usecase

---
+++ src/data/usecases/image/CreateImageUseCase.ts
@@ -0,0 +1,28 @@
+import { IImageRepository } from "@/data/protocols/repositories";
+import { Image } from "@/domain/entities";
+import { AlreadyExistsError } from "@/domain/errors";
+import { ICreateImageUseCase } from "@/domain/usecases/image";
+import { left, right } from "@/shared/error-handler/either";
+
+export class CreateImageUseCase implements ICreateImageUseCase {
+  private readonly imageRepository: IImageRepository;
+  constructor(imageRepository: IImageRepository) {
+    this.imageRepository = imageRepository;
+  }
+
+  async perform(data: ICreateImageUseCase.Input): ICreateImageUseCase.Output {
+    const images = await this.imageRepository.getByUser(data.user_id);
+
+    if (images.length > 0) {
+      const isExists = images.find((img) => img.url === data.url);
+
+      if (isExists) {
+        return left(new AlreadyExistsError("Image"));
+      }
+    }
+
+    const result = await this.imageRepository.create(data);
+
+    return right(result);
+  }
+}
