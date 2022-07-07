1196a574cc69e40d3e8bcd64b33729dce123c590
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 09:33:28 +0200

    feat: create router of login user

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,10 +1,9 @@
-import { loginUserFactory } from "@/main/factories/user/loginUserfactory";
+import { loginUserFactory, signupUserFactory } from "@/main/factories/user";
 import { Router } from "express";
 
 const userRoutes = Router();
 
-userRoutes.post("/signup", (request, response) =>
-  loginUserFactory().handle(request, response)
-);
+userRoutes.post("/signup", (request, response) => loginUserFactory().handle(request, response));
+userRoutes.post("/login", (request, response) => signupUserFactory().handle(request, response));
 
 export { userRoutes };
