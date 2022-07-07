62a021bc571dcb275edbfe7bd75dc7d1d1f5694f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:47:05 +0200

    feat: create factory user

--- src/main/factories/user/index.ts
+++ src/main/factories/user/index.ts
@@ -1,9 +1,8 @@
-import { SignupUserUseCase } from "@/data/usecases";
-import { LoginUserUseCase } from "@/data/usecases/LoginUserUseCase";
+import { SignupUserUseCase, LoginUserUseCase, LoadAccountByToken } from "@/data/usecases/user";
 import { BcryptAdapter, JwtAdapter } from "@/infrastructure/cryptography";
 import { UserRepository } from "@/infrastructure/mongodb";
-import { SignupUserController } from "@/presentation/controllers/user";
-import { LoginUserController } from "@/presentation/controllers/user/LoginUserController";
+import { SignupUserController, LoginUserController } from "@/presentation/controllers/user";
+import { AuthMiddleware } from "@/presentation/middlewares";
 import { env } from "@/shared/env";
 
 const secret = env.secret;
@@ -18,9 +17,23 @@ export const loginUserFactory = function () {
   const loginUserController = new LoginUserController(loginUserUseCase);
   return loginUserController;
 };
+
 export const signupUserFactory = function () {
   const loginUserUseCase = new SignupUserUseCase(userRepository, bcryptAdapter);
 
   const loginUserController = new SignupUserController(loginUserUseCase);
   return loginUserController;
+};
+
+export const loadAccountByToken = function () {
+  const loadAccountByTokenUseCase = new LoadAccountByToken(jwtAdapter, userRepository);
+
+  const loadAccountByToken = new AuthMiddleware(loadAccountByTokenUseCase);
+  return loadAccountByToken;
+};
+
+export const authMiddlewareFactory = function () {
+  const loadAccountByToken = new LoadAccountByToken(jwtAdapter, userRepository);
+  const authMiddleware = new AuthMiddleware(loadAccountByToken);
+  return authMiddleware;
 };
