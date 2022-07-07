23484616ac62f1dc9f500ab7dc954ad7e76d3c9c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:47:38 +0200

    feat: add router auth

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,9 +1,10 @@
-import { loginUserFactory, signupUserFactory } from "@/main/factories/user";
+import { authMiddlewareFactory, loginUserFactory, signupUserFactory } from "@/main/factories/user";
 import { Router } from "express";
 
 const userRoutes = Router();
 
-userRoutes.post("/signup", (request, response) => loginUserFactory().handle(request, response));
-userRoutes.post("/login", (request, response) => signupUserFactory().handle(request, response));
+userRoutes.post("/signup", (request, response) => signupUserFactory().handle(request, response));
+userRoutes.post("/login", (request, response) => loginUserFactory().handle(request, response));
+userRoutes.post("/auth", (request, response) => authMiddlewareFactory().handle(request, response));
 
 export { userRoutes };
