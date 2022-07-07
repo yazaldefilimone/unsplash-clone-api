fc9dd7115b9a5f0d5a827108a76e0f4f333c89cf
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 09:32:45 +0200

    feat: create login user controller

---
+++ src/presentation/controllers/user/LoginUserController.ts
@@ -0,0 +1,23 @@
+import { ILoginUserUseCase } from "@/domain/usecases/user";
+import { Request, Response } from "express";
+
+export class LoginUserController {
+  private readonly loginUserUseCase: ILoginUserUseCase;
+  constructor(loginUserUseCase: ILoginUserUseCase) {
+    this.loginUserUseCase = loginUserUseCase;
+  }
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { body } = request;
+      console.log({ body });
+      const result = await this.loginUserUseCase.perform(body);
+
+      if (result.isLeft()) {
+        return response.status(401).json({ message: result.value.message });
+      }
+      return response.status(201).send(result.value);
+    } catch (error: any) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
