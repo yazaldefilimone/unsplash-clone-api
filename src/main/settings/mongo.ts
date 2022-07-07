71f9bbef176345bbda14e0fcf18c43d681334316
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:49:35 +0200

    feat; remove credetials

--- src/main/settings/mongo.ts
+++ src/main/settings/mongo.ts
@@ -1,12 +1,6 @@
 import { env } from "@/shared/env";
 import mongoose, { mongo } from "mongoose";
 
-const user = "yazalde";
-const password = encodeURIComponent("react");
-const settings = {
-  url: `mongodb+srv://${user}:react@cluster0.duxminp.mongodb.net/?retryWrites=true&w=majority`,
-};
-
 export const SettinfMongoConnect = async () => {
-  await mongoose.connect(settings.url);
+  await mongoose.connect(env.mongodb.url);
 };
