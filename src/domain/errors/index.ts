db5d50b465c63f93053a5c6be256a16cf963a2e0
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 09:04:10 +0200

    feat: create unexpected error domain

---
+++ src/domain/errors/index.ts
@@ -0,0 +1,5 @@
+export * from './invalid-param-error';
+export * from './unexpected-error';
+export * from './email-in-use-error';
+export * from './access-denied-error';
+export * from './not-found-error';
