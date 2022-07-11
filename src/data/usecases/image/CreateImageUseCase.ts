0df11a86a1e35c73548acd22f4ccbb5c89f5b434
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-11 09:28:47 +0200

    feat: implementation validation

--- src/data/usecases/image/CreateImageUseCase.ts
+++ src/data/usecases/image/CreateImageUseCase.ts
@@ -1,6 +1,6 @@
 import { IImageRepository } from "@/data/protocols/repositories";
-import { Image } from "@/domain/entities";
-import { AlreadyExistsError } from "@/domain/errors";
+import { Image, User } from "@/domain/entities";
+import { AlreadyExistsError, InvalidParamError } from "@/domain/errors";
 import { ICreateImageUseCase } from "@/domain/usecases/image";
 import { left, right } from "@/shared/error-handler/either";
 
@@ -11,6 +11,12 @@ export class CreateImageUseCase implements ICreateImageUseCase {
   }
 
   async perform(data: ICreateImageUseCase.Input): ICreateImageUseCase.Output {
+    const label = new User().createName(data.label);
+
+    if (label.isLeft()) {
+      return left(new InvalidParamError(data.label));
+    }
+
     const images = await this.imageRepository.getByUser(data.userId);
 
     if (images.length > 0) {
